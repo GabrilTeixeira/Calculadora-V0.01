c5f10cfa951cdedb16a3075b4954bb8870a7d2b7
Gabriel Teixeira <gabriel.teixeira@Duque.g12.br>
2023-09-21 21:13:48 -0300

    Criação da tela da calculadora

---
+++ src/main/java/br/g12/duque/calculadora/Calculadora.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.g12.duque.calculadora;
+
+/**
+ *
+ * @author 08098
+ */
+public class Calculadora {
+    public static void main(String[] args) {
+        FrmCalculadora calc = new FrmCalculadora();
+        calc.setLocationRelativeTo(calc);
+        calc.setVisible(true);
+       
+    }
+}
